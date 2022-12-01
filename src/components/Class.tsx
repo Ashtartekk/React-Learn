93000a531d67a46408c955cc2ec17b3ea8adcf80
AshtarteKk <865189421@qq.com>
2022-12-01 16:58:51 +0800

    new Login page

--- src/components/Class.tsx
+++ src/components/Class.tsx
@@ -4,18 +4,27 @@ import React, { PureComponent } from "react";
 function addAge(Target: Function) {
   Target.prototype.name = 'AshtarteKk'
   Target.prototype.age = 24
+  Target.prototype.obj = {name:'Ash',age:24}
+}
+const handleClick = ()=>{
+  console.log(111)
 }
 // 使用装饰圈
 @addAge
 class Class extends PureComponent {
-
+  name?:string
   age?: number
-
+  obj?:{
+    name:string,
+    age:number
+  }
   render() {
     return (
       <>
-      <h2>Name:{this.age}</h2>
+      <h2>Name:{this.name}</h2>
       <h2>Age:{this.age}</h2>
+      <div>object.name:{this.obj?.name}</div>
+      <div onClick={handleClick()}>object.age:{this.obj?.age}</div>
       </>
     )
   }
