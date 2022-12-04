c8779e99459d20558d19cb555732bff4d37382db
AshtarteKk <865189421@qq.com>
2022-12-04 21:01:21 +0800

    map

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,49 @@ import './app.css'
 import Class from '../src/components/Class'
 import Home from '../src/views/Home'
 
+//JSX是什么？
+//概念:JSX是JavaScript XML(HTML)的缩写，表示JS代码中书写HTML结构
+//作用:React中创建HTML结构(页面UI结构)
+//优势:1.采用类似于HTML的语法，降低学习成本，会HTML就会JSX
+//2.充分利用JS自身的可编程能力创建HTML结构
+//注意：JSX并不是标准的JS语法，是JS的语法扩展，浏览器默认是不识别的，脚手架内置的@babel/plugin-transform-react-jsx包，用来解析该语法
+
+const name = 'AshtarteKk' //识别常规的变量
+const getAge = ()=>{  //原生js方法的调用
+  return 18
+}
+let flag = true
+const handleFlag = ()=>{
+  flag = !flag
+}
+
+//react如何完成列表渲染
+//技术方案:map 重复渲染的是哪个模板就return谁
+//注意事项:遍历列表时同样需要一个类型为number/string不可重复的key 提高diff性能
+const songs = [
+  { id:1,name:'富士山下'},
+  { id:2,name:'单车'},
+  { id:3,name:'孤勇者'},
+]
 
 function App() {
   return(
     <div>
         <h2>webpack5-react-ts</h2>
+        <br />
+        <h2>name:{name}</h2>
+        <h2>age:{getAge()}</h2>
+        <h2>{flag ? 'true' : 'false'}</h2>
+        <button onClick={handleFlag}>Flag</button>
         {/* <img src={webpack} alt="" /> */}
+        <br />
         <Class/>
         <Home/>
+        <div>
+          <ul>
+            { songs.map(song=> <li key={song.id}>id:{song.id},name:{song.name}</li>) }
+          </ul>
+        </div>
     </div>
   ) 
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './app.less'
+import React from "react"; //React:框架核心包
+import { createRoot } from "react-dom/client"; //ReactDOM:专门做渲染相关的包
+import App from "./App"; //引入根组件
+import "./app.less"; //应用全局样式文件
 
+console.log("NODE_ENV", process.env.NODE_ENV);
+console.log("BASE_ENV", process.env.BASE_ENV);
 
-console.log('NODE_ENV',process.env.NODE_ENV)
-console.log('BASE_ENV',process.env.BASE_ENV)
+//渲染跟组件APP 到 一个id为root的dom节点上
 
-const root = document.getElementById('root');
-if(root) {
-  createRoot(root).render(<App />)
+const root = document.getElementById("root");
+if (root) {
+  createRoot(root).render(
+    //<React.StrictMode></React.StrictMode> 这个是React的严格模式节点
+    //useEffect的执行时机
+    <App />
+  );
 }
