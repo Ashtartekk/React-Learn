1aec799ddb5aec6308c1f48370d32194f3c9ec62
AshtarteKk <865189421@qq.com>
2022-12-05 15:18:05 +0800

    子传父

--- src/components/Children.tsx
+++ src/components/Children.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 // class Children extends React.Component{
 //     render(){
@@ -8,11 +8,18 @@ import React from 'react'
 //     }
 // }
 
-function Children(props:any){
-    console.log(props.msg)
-    return (
-        <div>子组件:1111{props.msg}1111</div>
-    )
+function Children(props: any) {
+  function handleToFather() {
+    // 调用父组件传递过来的回调函数 并注入参数
+    props.changeMsg("子传父的数据");
+  }
+  return (
+    <>
+      <div>子组件:1111{props.msg}1111</div>
+      <div>2222 {props.msg}2222</div>
+      <button onClick={handleToFather}>change</button>
+    </>
+  );
 }
 
-export default Children
+export default Children;
--- src/components/Father.tsx
+++ src/components/Father.tsx
@@ -4,17 +4,27 @@ import Children from './Children'
 class Father extends React.Component{
     state = {
         Name:'AshtarteKk',
-        Age:24
+        Age:24,
+        GetMsg:'111'
     }
+    changeMessage = (newMsg:string) => {
+        console.log('子组件传过来的数据:',newMsg)
+        this.setState({
+            GetMsg: newMsg
+        })
+      }
     render(){
         return(
             <div>
                 <p>父组件</p>
-                <Children msg={this.state.Name} />
-                <Children msg={this.state.Age} />
+                {/* <Children msg={this.state.Name} />
+                <Children msg={this.state.Age} /> */}
+                <Children msg={this.state.GetMsg} changeMsg={this.changeMessage} />
+                <div>GetMsg:{this.state.GetMsg}</div>
             </div>
         )
     }
 }
 
 export default Father
+
