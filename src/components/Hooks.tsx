9bdd7d15985484a7b4eed19e407072d2f9bf9b1e
AshtarteKk <865189421@qq.com>
2022-12-07 23:43:10 +0800

    Hook进阶

--- src/components/Hooks.tsx
+++ src/components/Hooks.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react";
 
 function Hooks() {
   const [number, setNumber] = useState(() => {
-    //这里写计算逻辑 return '计算之后的初始值'
+    //这里写计算逻辑 return '计算之后的初始值' 相当于计算属性
     return 1 + 5;
   });
   return (
